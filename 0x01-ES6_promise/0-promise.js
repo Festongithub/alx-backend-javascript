a3e1b5b0a99a92d687add2008498fe315266e293
Festongithub <fkemboik@gmail.com>
2024-08-07 15:15:46 +0000

    Promises made must be fulllfiled

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 export default function getResponseFromAPI() {
 	return new Promise((resolve, reject) => {});
 }
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
