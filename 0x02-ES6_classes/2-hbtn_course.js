b26924e8b35a4c4a6a9077f5a5fb25059c56001e
Festongithub <fkemboik@gmail.com>
2024-08-09 09:00:26 +0000

    Classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,38 +1,38 @@
 export default class HolbertonCourse {
-	constructor (name, length, students) {
-		if(typeof name !== 'string') throw new TypeError('name must be a string');
-		if(!Number.isInteger(length))throw new TypeError('length must be a number');
-		if (!Array.isArray(students))throw new TypeError('students must be an array');
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (!Number.isInteger(length)) throw new TypeError('length must be a number');
+    if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
 
-		this._name = name;
-		this._length = length;
-		this._students = students;
-	}
-	
-	get name() {
-		return this._name;
-	}
-	
-	set name(newval) {
-		if (typeof newval !== 'string')throw new TypeError('name must be a string');
-		this._name = newval;
-	}
-	
-	get length() {
-		return this._length;
-	}
-	
-	set length(newval) {
-		if(!Number.isInteger(newval))throw new TypeError('length must be a number');
-		this._length = newval;
-	}
-	
-	get students() {
-		return this._students;
-	}
-	
-	set students(newval) {
-		if(!Array.isArray(newval)) throw new TypeError('students must be an arrsy');
-		this._students = newval;
-	}
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') throw new TypeError('name must be a string');
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (!Number.isInteger(val)) throw new TypeError('length must be a number');
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
+    this._students = val;
+  }
 }
