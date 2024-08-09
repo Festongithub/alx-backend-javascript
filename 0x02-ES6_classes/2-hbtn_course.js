ee8f2642ca5983041634da85887ed0c5a5a064c4
Festongithub <fkemboik@gmail.com>
2024-08-09 07:47:02 +0000

    My classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+	constructor (name, length, students)
+	{
+		if(typeof name !== 'string') throw new TypeError('name must be a string');
+		if(!Number.isInteger(length))throw new TypeError('length must be a number');
+		if (!Array.isArray(students))throw new TypeError('students must be an array');
+
+		this._name = name;
+		this._length = length;
+		this._students = students;
+	}
+
+		get name()
+		{
+			return this._name;
+		}
+
+		set name(newName)
+		{
+			if (typeof newName !== 'string')throw new TypeError('name must be a string');
+			this._name = newName;
+		}
+
+		get length()
+		{
+			return this._length;
+		}
+
+		set length(newLength)
+		{
+			if(!Number.isInteger(newLength))throw new TypeError('length must be a number');
+			this._length = newLength;
+		}
+
+		get students()
+		{
+			return this._students;
+		}
+
+		set students(newst)
+		{
+			if(!Array.isArray(newst)) throw new TypeError('students must be an arrsy');
+			this._students;
+		}
+}
