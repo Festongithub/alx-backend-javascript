ae055ca390b64df3a7a7678724b0527b386564fa
Festongithub <fkemboik@gmail.com>
2024-08-13 08:57:14 +0000

    Data Manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
-	if (!startString || typeof startString !== 'string') {
-		return '';
-	}
-	
-	return Array.from(set)
-		.filter(value => value.startsWith(startString))
-		.map(value => value.slice(startString.length))
-		.join('-');
+  if (startString === undefined || startString.length === 0) {
+	  return '';
+  }
+  return [...set]
+    .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
+    .map((value) => (value !== undefined ? value.slice(startString.length) : ''))
+    .join('-');
 }
