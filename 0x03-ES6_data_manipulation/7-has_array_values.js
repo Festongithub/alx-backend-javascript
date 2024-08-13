67b5558be5abbebf46ee861d00e4425f214a4ac1
Festongithub <fkemboik@gmail.com>
2024-08-13 08:38:36 +0000

    Data Manipulation

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,10 @@
+/* eslint-disable array-callback return */
 export default function hasValuesFromArray(set, array) {
-  return array.every(element => set.has(element));
+  let test = true;
+  array.map((x) => {
+     if (!set.has(x)) {
+	test = false;
+     }
+  });
+  return verdict;
 }
