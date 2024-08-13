16db10b6a9fe07c35f148ffafeec32907a33254c
Festongithub <fkemboik@gmail.com>
2024-08-13 09:17:39 +0000

    Data Manipulation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(groceries) {
-	if (!(groceries instanceof Map)) {
-		throw new Error('Cannot process');
-	}
-	
-	groceries.forEach((quantity, item) => {
-		if (quantity === 1) {
-			groceries.set(item, 100);
-		}
-	});
-	return groceries;
+  if (groceries instanceof Map) {
+    for (const [Key, value] of groceries) {
+      if (value === 1) {
+	      groceries.set(Key, 100);
+      }
+    }
+    return groceries;
+  }
+  
+  throw new Error('Cannot process');
 }
