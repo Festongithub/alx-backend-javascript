8545e3487bed53af3f9c75f50ede9bddc3b01c4d
Festongithub <fkemboik@gmail.com>
2024-10-01 09:51:19 +0000

    Building on Node

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+const app = express()
+const hostname = '127.0.0.1'
+const port = 1245
+
+app.get('/', (req, res) => {
+	res.send('Hello Holberton School!')
+});
+
+app.listen(hostname, port, () => {
+	console.log(`Example app listening on http://${hostname}:${port}`)
+});
