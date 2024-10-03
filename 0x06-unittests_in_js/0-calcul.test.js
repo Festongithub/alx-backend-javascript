f8e23eb22248bb420a5cff90a7113bec92c844d5
Festongithub <fkemboik@gmail.com>
2024-10-03 15:04:57 +0000

    built for the test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,12 +3,25 @@
 const assert = require('assert')
 const calculateNumber = require('./0-calcul')
 
-
-describe('calculateNumber', function() {
-
-	it('should return 4 when inputs are 1 and 4')
-	assert.strictEqual(1, 3), 4);
-});
-        it('should return 5 for (rounded 3.2 and 1.8)', function())
-        assert(.strictEqual(3.2, 1.8), 5);
+describe('Tests calculateNumber function', function() {
+	describe('calculateNumber', function() {
+		it('should return 4 when inputs are 1 and 3', function() {
+			assert.strictEqual(calculateNumber(1, 3), 4);
+		});
+	});
+	describe('calaculate two floats integers', function() {
+		it('should return 5 for (rounded 3.2 and 1.8)', function() {
+			assert.strictEqual(calculateNumber(3.2, 1.8), 5);
+		});
+	});
+	describe('calaculate float and an integers', function() {
+                it('should return 5 for (rounded 3.9 and 1)', function() {
+                        assert.strictEqual(calculateNumber(3.9, 1), 5);
+                });
+        });
+	describe('calaculate two floats integers', function() {
+                it('should return 5 for (rounded 5.2 and 1.8)', function() {
+                        assert.strictEqual(calculateNumber(5.2, 1.8), 7);
+                });
+        });
 });
