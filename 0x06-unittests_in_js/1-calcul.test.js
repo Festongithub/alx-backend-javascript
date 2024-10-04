5f642b02636baa32be34593c8374cdda1949ea02
root <fkemboik@gmail.com>
2024-10-04 08:25:27 +0000

    Built for test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,27 +1,25 @@
-#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
 
-const assert = require('assert')
-const calculateNumber = require('./1-calcul')
-
-describe('Tests calculateNumber function', function() {
-	describe('calculateNumber', function() {
-		it('should return 4 when inputs are 1 and 3', function() {
-			assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
-		});
-	});
-	describe('calaculate two floats integers', function() {
-		it('should return 2 for (rounded 3.2 and 1.8)', function() {
-			assert.strictEqual(calculateNumber('SUBTRACT',3.2, 1.6), 2);
-		});
-	});
-	describe('calaculate float and an integers', function() {
-                it('should return 3.9 for (rounded 3.9 and 1)', function() {
-                        assert.strictEqual(calculateNumber('DIVIDE',3.9, 1), 4);
-                });
-        });
-	describe('calaculate two floats integers', function() {
-                it('should return Error for (rounded 5.2 and 1.8)', function() {
-                        assert.strictEqual(calculateNumber('DIVIDE',5.2, 0),'Error');
-                });
-        });
+describe('tests advanced calculateNumber function:', () => {
+	    describe('tests SUM function:', () => {
+		            it('returns 6.', () => {
+				                assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+				            });
+		        });
+	    describe('tests SUBTRACT function:', () => {
+		            it('returns -4.', () => {
+				                assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+				            });
+		        });
+	    describe('tests DIVIDE function:', () => {
+		            it('returns 0.2.', () => {
+				                assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+				            });
+		        });
+	    describe('tests DIVIDE function W 0:', () => {
+		            it("returns 'Error'.", () => {
+				                assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+				            });
+		        });
 });
