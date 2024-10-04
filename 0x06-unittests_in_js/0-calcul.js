61290f33efaf6999be87f34aa50646646ec1ea23
root <fkemboik@gmail.com>
2024-10-04 08:20:13 +0000

    Built for test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,10 @@
 // Calculation function.
 
+
 function calculateNumber(a, b) {
-  firstInt = Math.round(a);
-  secondInt = Math.round(b);
-  return firstInt + secondInt;
+    firstInt = Math.round(a);
+    secondInt = Math.round(b);
+    return firstInt + secondInt;
 }
 
 module.exports = calculateNumber;
