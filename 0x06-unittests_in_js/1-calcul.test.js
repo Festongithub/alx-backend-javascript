0652638793534f8c469ec4d55dc6f59503054f02
root <fkemboik@gmail.com>
2024-10-04 08:48:54 +0000

    Built for test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -5,7 +5,7 @@ describe('tests advanced calculateNumber function:', () => {
   describe('tests SUM function:', () => {
     it('returns 6.', () => {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-	     });
+    });
   });
   describe('tests SUBTRACT function:', () => {
     it('returns -4.', () => {
@@ -15,11 +15,6 @@ describe('tests advanced calculateNumber function:', () => {
   describe('tests DIVIDE function:', () => {
     it('returns 0.2.', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-    });
-  });
-  describe('tests DIVIDE function W 0:', () => {
-    it("returns 'Error'.", () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
   });
 });
