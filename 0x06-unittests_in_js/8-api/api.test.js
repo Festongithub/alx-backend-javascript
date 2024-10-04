899a337a2e988c9116a35e7453f170a81c589eac
root <fkemboik@gmail.com>
2024-10-04 12:19:47 +0000

    Built for test

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,9 +5,10 @@ describe('Integration Testing', function() {
 	describe('GET /', function () {
 		it('returns StatusCode: 200 | Body: Welcome to the payment system', (done) => {
 			const options = {
-				url='http://localhost:7865',
-				method: 'GET',
+				url:'http://localhost:7865',
+				method:'GET',
 			};
+
 			request(options, function(error, response, body) {
 				expect(response.statusCode).to.equal(200);
 				expect(body).to.equal('Welcome to the payment system');
